aa3b86934ee24c88f6fd71bf424f074df77f59b9
Skylar Kelty <skylarkelty@gmail.com>
2015-01-17 09:59:06 +0000

    Add base code

---
+++ lang/en/block_sort.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_sort', language 'en'
+ *
+ * @package    block_sort
+ * @copyright  2015 Onwards Skylar Kelty
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ */
+
+$string['pluginname'] = 'Sort';
+$string['sort:addinstance'] = 'Add a new sort block';
+
+$string['sort'] = 'Sort';
+$string['sortcourse'] = 'Sort this course';
+$string['sorting'] = 'Sorting';
+$string['sortwarn'] = 'Warning! Once you have sorted the course, you can undo it but only until your first edit. After you have edited anything on the course, you cannot undo this action!';
+$string['undo'] = 'Undo';
